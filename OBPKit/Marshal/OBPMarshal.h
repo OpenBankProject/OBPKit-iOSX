f517b8c9738ce7154cff300b659f2e1dc13e6ad4
Torsten Louland <torsten.louland@satisfyingstructures.com>
2016-06-09 22:43:13 +0200

    In OBPMarshal, support POST/PUT/DELETE and more… (interface change!)
    - add support for POST/PUT/DELETE
      - allow POST/PUT data as raw, JSON, form
    - add support for extra headers
    - change default expected class to NSDictionary
    - allow option of several possible response status codes
    - support no-auth calls (public resources)
    - add verbose logging mode (defaults write -bool bundle-id OBPMarshalVerbose YES/NO)

--- OBPKit/Marshal/OBPMarshal.h
+++ OBPKit/Marshal/OBPMarshal.h
@@ -23,26 +23,50 @@ NS_ENUM(NSInteger) {	OBPMarshalErrorUnexpectedResourceKind		= 4192,
 						OBPMarshalErrorUnexpectedResult				= 4193,
 };
 
-static NSString* const	OBPMarshalOptionExpectClass					= @"expectClass"; ///< OBPMarshalOptionExpectClass key for options dictionary, value of type Class is the expected class of the deserialized JSON object; mismatch is treated as OBPMarshalErrorUnexpectedResourceKind
-static NSString* const	OBPMarshalOptionExpectStatus				= @"expectStatus"; ///< OBPMarshalOptionExpectStatus key for options dictionary, value of type NSNumber is the expected normal response status if other than 200 (the default).
-static NSString* const	OBPMarshalOptionDeserializeJSON				= @"deserializeJSON"; ///< OBPMarshalOptionDeserializeJSON key for options dictionary, value of type NSNumber interpretted as BOOL and indicating whether to deserialize the response body as a JSON object: value YES is the same as omitting the option; use NO to suppress.
+static NSString* const	OBPMarshalOptionOnlyPublicResources			= @"onlyPublic"; ///< OBPMarshalOptionOnlyPublicResources key for options dictionary, value of type NSNumber interpreted as BOOL, where value YES indicates omit authorization in order to act on only the publicly available resources and value NO (default when omitted) indicates add authorisation so as to act on both the privately available resources of the authorised user and the publicly available resources.
+static NSString* const	OBPMarshalOptionSendDictAsForm				= @"serializeToJSON"; ///< OBPMarshalOptionSendDictAsForm key for options dictionary, value of type NSNumber interpreted as BOOL, where value YES indicates send a dictionary payload as "application/x-www-form-urlencoded", and value NO (same as omitting the option) indicates serialize the payload (for POST/PUT...) as a JSON object; a payload of type NSData is always sent as raw data.
+static NSString* const	OBPMarshalOptionExtraHeaders				= @"extraHeaders"; ///< OBPMarshalOptionExtraHeaders key for options dictionary, value of type dictionary, containing extra header key-value pairs that are acceptable for the particular API call, e.g. for sorting, and subranging by date and/or ordinal; header values supplied as NSDate and NSNumber will be converted to correct string values.
+static NSString* const	OBPMarshalOptionExpectClass					= @"expectClass"; ///< OBPMarshalOptionExpectClass key for options dictionary, value of type Class is the expected class of the deserialized JSON object, pass [NSNull null] or [NSNull class] to signify no fixed expectation; if omited, then desrialized object is expected to be an NSDictionary; mismatch is treated as OBPMarshalErrorUnexpectedResourceKind
+static NSString* const	OBPMarshalOptionExpectStatus				= @"expectStatus"; ///< OBPMarshalOptionExpectStatus key for options dictionary, value of type NSNumber or array of NSNumber giving the expected normal response status code(s); when omitted, the default expectations are 201 for POST, 204 for DELETE, 200 for others.
+static NSString* const	OBPMarshalOptionDeserializeJSON				= @"deserializeJSON"; ///< OBPMarshalOptionDeserializeJSON key for options dictionary, value of type NSNumber interpreted as BOOL and indicating whether to deserialize the response body as a JSON object: value YES is the same as omitting the option; use NO to suppress.
 static NSString* const	OBPMarshalOptionErrorHandler				= @"errorHandler"; ///< OBPMarshalOptionErrorHandler key for options dictionary, value of type HandleOBPMarshalError block gives alternative error handler to the standard handler.
 
 
 
 typedef void(^HandleOBPMarshalError)(NSError* error, NSString* path);
-typedef void(^HandleOBPMarshalData)(id deserializedJSONObject, NSString* responseBody);
+typedef void(^HandleOBPMarshalData)(id deserializedObject, NSString* responseBody);
 
 
 
-/// Class OBPMarshal helps you marshal resources through the OBP API. Paths are always relative to the OBP API base. There must always be a supplied or default error handler. You can obtain a default instance from an OBPSession instance, or create your own.
+/** Class OBPMarshal helps you marshal resources through the OBP API with get (GET), create (POST), update (PUT) and delete (DELETE) operations. Paths are always relative to the OBP API base. There must always be a supplied error handler or a default error handler. You can obtain a default instance from an OBPSession instance, or create your own.
+
+	An OBPMarshal instance will:
+	
+	-	use private API calls, i.e. with authorisation by the session object. You can make public API call (no authorisation) by adding OBPMarshalOptionOnlyPublicResources : @YES to your options dictionary.
+
+	-	use its own error handler, unless you supply one as a parameter, or by adding OBPMarshalOptionErrorHandler : yourErrorHandler to the options dictionary; the parameter is chosen in preference to the option.
+
+	-	send a resource (create, update) supplied as NSData as the raw data, and a resource supplied as any other valid JSON root object kind as its serialised JSON description. To send a dictionary resource as a form, include OBPMarshalOptionSendDictAsForm : @YES in your options dictionary.
+	
+	-	accept a response with status code 201 for create (POST), 204 for DELETE, and 200 for get and update (GET, PUT), and will reject the response if the status is different. To specify one or more status codes to accept instead of the defaults, add OBPMarshalOptionExpectStatus key with a single NSNumber or an NSArray of NSNumber to your options dictionary, e.g. OBPMarshalOptionExpectStatus : @212, or OBPMarshalOptionExpectStatus : @[@201, @212].
+	
+	-	if the response body is non-empty, expect it to be a serialized object in JSON format, will deserialize it for you and will reject the response if deserialised object is not a dictionary. To prevent deserialisation, add OBPMarshalOptionDeserializeJSON : @NO to your options dictionary. To expect a different class of JSON root object include OBPMarshalOptionExpectClass : class in your options dictionary. To suppress class checking, add OBPMarshalOptionExpectClass : [NSNull null].
+	
+	To add extra headers that modify the action of the call, add OBPMarshalOptionExtraHeaders : headerDictionary to your options dictionary. For example, to page transactions with get /banks/BANK_ID/accounts/ACCOUNT_ID/VIEW_ID/transactions, you can add OBPMarshalOptionExtraHeaders : @{@"obp_limit":@(chunkSize), @"obp_offset":@(nextChunkOffset)}. Note that OBPMarshal will convert any NSDate values you pass to strings using OBPDateFormatter.
+*/
 @interface OBPMarshal : NSObject
 @property (nonatomic, strong) HandleOBPMarshalError errorHandler; ///< Get/set a default error handler block for this instance.
 @property (nonatomic, weak, readonly) OBPSession* session; ///< Get the session object that this instance exclusively works with, identifying the OBP server with which it communicates.
 
 - (instancetype)initWithSessionAuth:(OBPSession*)session; ///< Designated initialiser. session parameter is mandatory. Sets a default error handler which simply logs the error in Debug builds.
 
-- (BOOL)getResourceAtAPIPath:(NSString*)path withOptions:(nullable NSDictionary*)options forHandler:(HandleOBPMarshalData)handler; ///< Request the resource at the supplied path from the API base, passing the result to handler, or errors to the default error handler of this instance, or as modified by options. \param path identifies the resource to get, relative to the API base URL. \param options may supply values for any of the following keys: OBPMarshalOptionExpectClass, OBPMarshalOptionExpectStatus, OBPMarshalOptionDeserializeJSON, OBPMarshalOptionErrorHandler; see the description for each key. \returns YES if the request was launched, or NO if the session or parameters were invalid.
+- (BOOL)getResourceAtAPIPath:(NSString*)path withOptions:(nullable NSDictionary*)options forResultHandler:(HandleOBPMarshalData)resultHandler orErrorHandler:(nullable HandleOBPMarshalError)errorHandler; ///< Request the resource at path from API base (GET), passing the result to handler, or errors to the error handler. \param path identifies the resource to get, relative to the API base URL. \param options may supply key-value pairs to customise behaviour. \returns YES if the request was launched, or NO if the session or parameters were invalid. \sa See the class description for details of default behaviour and how to override using the options parameter.
+
+- (BOOL)updateResource:(id)resource atAPIPath:(NSString*)path withOptions:(nullable NSDictionary*)options forResultHandler:(HandleOBPMarshalData)resultHandler orErrorHandler:(nullable HandleOBPMarshalError)errorHandler; ///< Update the resource at path from API base (PUT), passing the result to handler, or errors to the error handler. \param path identifies the resource to update, relative to the API base URL. \param options may supply key-value pairs to customise behaviour. \returns YES if the request was launched, or NO if the session or parameters were invalid. \sa See the class description for details of default behaviour and how to override using the options parameter.
+
+- (BOOL)createResource:(id)resource atAPIPath:(NSString*)path withOptions:(nullable NSDictionary*)options forResultHandler:(HandleOBPMarshalData)resultHandler orErrorHandler:(nullable HandleOBPMarshalError)errorHandler; ///< Create a resource at path from API base (POST), passing the result to handler, or errors to the error handler. \param path identifies the resource to create, relative to the API base URL. \param options may supply key-value pairs to customise behaviour. \returns YES if the request was launched, or NO if the session or parameters were invalid. \sa See the class description for details of default behaviour and how to override using the options parameter.
+
+- (BOOL)deleteResourceAtAPIPath:(NSString*)path withOptions:(nullable NSDictionary*)options forResultHandler:(HandleOBPMarshalData)resultHandler orErrorHandler:(nullable HandleOBPMarshalError)errorHandler; ///< Delete the resource at path from API base (DELETE), passing the result to handler, or errors to the error handler. \param options may supply key-value pairs to customise behaviour. \returns YES if the request was launched, or NO if the session or parameters were invalid. \sa See the class description for details of default behaviour and how to override using the options parameter.
 
 @end
 
--- OBPKit/Marshal/OBPMarshal.m
+++ OBPKit/Marshal/OBPMarshal.m
@@ -15,6 +15,24 @@
 #import "OBPSession.h"
 #import "NSString+OBPKit.h"
 #import "OBPLogging.h"
+#import "OBPDateFormatter.h"
+
+
+
+typedef NS_ENUM(uint8_t, OBPMarshalVerb)
+{
+	eOBPMarshalVerb_GET,
+	eOBPMarshalVerb_PUT,
+	eOBPMarshalVerb_POST,
+	eOBPMarshalVerb_DELETE,
+
+	eOBPMarshalVerb_count
+};
+
+
+
+NSString* NSStringDescribingNSURLRequest(NSURLRequest* request);
+NSString* NSStringDescribingNSURLResponseAndData(NSHTTPURLResponse* response, NSData* data);
 
 
 
@@ -31,57 +49,191 @@
 }
 - (void)setDefaultErrorHandler
 {
-	NSString* APIBase = _session.serverInfo.APIBase;
+	__weak __typeof(self) self_ifStillAlive = self;
 	_errorHandler =
 		^(NSError* error, NSString* path)
 		{
-			OBP_LOG(@"Request for resource at path %@ served by %@ got error %@", path, APIBase, error);
+			OBP_LOG(@"Request for resource at path %@ served by %@ got error %@", path, self_ifStillAlive.session.serverInfo.APIBase, error);
 		};
 }
-- (BOOL)getResourceAtAPIPath:(NSString*)path withOptions:(NSDictionary*)options forHandler:(HandleOBPMarshalData)resultHandler
+- (BOOL)getResourceAtAPIPath:(NSString*)p withOptions:(NSDictionary*)o forResultHandler:(HandleOBPMarshalData)rh orErrorHandler:(HandleOBPMarshalError)eh
 {
+	if (eh == nil && o)
+		eh = o[@"errorHandler"];
+	return [self sendRequestVerb: eOBPMarshalVerb_GET withPayload: nil toAPIPath: p withOptions: o forResultHandler: rh orErrorHandler: eh];
+}
+- (BOOL)updateResource:(id)r atAPIPath:(NSString*)p withOptions:(NSDictionary*)o forResultHandler:(HandleOBPMarshalData)rh orErrorHandler:(HandleOBPMarshalError)eh
+{
+	if (eh == nil && o)
+		eh = o[@"errorHandler"];
+	return [self sendRequestVerb: eOBPMarshalVerb_PUT withPayload: r toAPIPath: p withOptions: o forResultHandler: rh orErrorHandler: eh];
+}
+- (BOOL)createResource:(id)r atAPIPath:(NSString*)p withOptions:(NSDictionary*)o forResultHandler:(HandleOBPMarshalData)rh orErrorHandler:(HandleOBPMarshalError)eh
+{
+	if (eh == nil && o)
+		eh = o[@"errorHandler"];
+	return [self sendRequestVerb: eOBPMarshalVerb_POST withPayload: r toAPIPath: p withOptions: o forResultHandler: rh orErrorHandler: eh];
+}
+- (BOOL)deleteResourceAtAPIPath:(NSString*)p withOptions:(NSDictionary*)o forResultHandler:(HandleOBPMarshalData)rh orErrorHandler:(HandleOBPMarshalError)eh
+{
+	if (eh == nil && o)
+		eh = o[@"errorHandler"];
+	return [self sendRequestVerb: eOBPMarshalVerb_DELETE withPayload: nil toAPIPath: p withOptions: o forResultHandler: rh orErrorHandler: eh];
+}
+- (BOOL)sendRequestVerb:(OBPMarshalVerb)verb
+			withPayload:(id)payload
+			  toAPIPath:(NSString*)path
+			withOptions:(NSDictionary*)options
+	   forResultHandler:(HandleOBPMarshalData)resultHandler
+		 orErrorHandler:(HandleOBPMarshalError)errorHandler
+{
+	OBPSession*				session = _session;
+	HandleOBPMarshalError	eh = errorHandler ?: _errorHandler;
+
+	if ((!session.valid && ![options[OBPMarshalOptionOnlyPublicResources] isEqual: @YES])
+	 || ![path length] || !resultHandler || !eh)
+		return NO;
+
 	NSString*				requestPath;
 	STHTTPRequest*			request;
 	STHTTPRequest __weak*	request_ifStillAround;
-	OBPSession*				session = _session;
-	HandleOBPMarshalError	errorHandler = options[@"errorHandler"];
-	HandleOBPMarshalError	eh = errorHandler ?: _errorHandler;
-	Class					xc = Nil;
+	NSString*				method;
+	Class					expectedDeserializedObjectClass = [NSDictionary class];
+	id						obj;
+	BOOL					onlyPublicResources = NO;
+	BOOL					sendDictAsForm = NO;
+	BOOL					serializeToJSON = YES;
 	BOOL					deserializeJSON = YES;
-	NSInteger				statusAcceptable = 200;
+	BOOL					verbose = [[NSUserDefaults standardUserDefaults] boolForKey: @"OBPMarshalVerbose"];
+	NSArray*				acceptableStatusCodes;
+	NSDictionary*			dict;
+	NSString*				key;
+	NSData*					data;
+	NSError*				error;
+	NSMutableDictionary*	moreHeaders = [NSMutableDictionary dictionary];
 
-	if (!session.valid || ![path length] || !resultHandler || !eh)
-		return NO;
-
-	if (options)
+	// Method
+	switch (verb)
 	{
-		xc = options[OBPMarshalOptionExpectClass];
-		id obj = options[OBPMarshalOptionDeserializeJSON];
-		if ([obj respondsToSelector: @selector(boolValue)])
-			deserializeJSON = [obj boolValue];
-		obj = options[OBPMarshalOptionExpectStatus];
-		if ([obj respondsToSelector: @selector(integerValue)])
-			statusAcceptable = [obj integerValue];
+		case eOBPMarshalVerb_GET:
+			method = @"GET";
+			acceptableStatusCodes = @[@200];
+			break;
+		case eOBPMarshalVerb_PUT:
+			method = @"PUT";
+			acceptableStatusCodes = @[@200];
+			break;
+		case eOBPMarshalVerb_POST:
+			method = @"POST";
+			acceptableStatusCodes = @[@201];
+			break;
+		case eOBPMarshalVerb_DELETE:
+			method = @"DELETE";
+			acceptableStatusCodes = @[@204];
+			break;
+		default:
+			return NO;
 	}
 
+	// Options
+	if (options)
+	{
+		// Expected class of object after deserialisation
+		obj = options[OBPMarshalOptionExpectClass];
+		if (obj)
+		if ([obj isEqual: [NSNull null]] || obj == [NSNull class])
+			expectedDeserializedObjectClass = nil;
+		else
+			expectedDeserializedObjectClass = obj;
+
+		// Make public calls? (suppress authorisation)
+		obj = options[OBPMarshalOptionOnlyPublicResources];
+		if ([obj respondsToSelector: @selector(boolValue)])
+			onlyPublicResources = [obj boolValue];
+
+		// Send payload as form?
+		obj = options[OBPMarshalOptionSendDictAsForm];
+		if ([obj respondsToSelector: @selector(boolValue)])
+			serializeToJSON = !(sendDictAsForm = [obj boolValue]);
+
+		// Expect reply body is JSON and deserialize?
+		obj = options[OBPMarshalOptionDeserializeJSON];
+		if ([obj respondsToSelector: @selector(boolValue)])
+			deserializeJSON = [obj boolValue];
+
+		// Expect non-default reply status code(s)?
+		obj = options[OBPMarshalOptionExpectStatus];
+		if ([obj respondsToSelector: @selector(integerValue)])
+			acceptableStatusCodes = @[obj];
+		else
+		if ([obj isKindOfClass: [NSArray class]])
+			acceptableStatusCodes = obj;
+
+		// Add extra headers?
+		dict = obj = options[OBPMarshalOptionExtraHeaders];
+		if ([obj isKindOfClass: [NSDictionary class]])
+		for (key in dict)
+		{
+			obj = dict[key];
+			if ([obj isKindOfClass: [NSDate class]])
+				moreHeaders[key] = [OBPDateFormatter stringFromDate: obj];
+			else
+				moreHeaders[key] = [obj description];
+		}
+	}
+
+	// Make the request and add its payload
 	requestPath = [session.serverInfo.APIBase stringForURLByAppendingPath: path];
 	request = [STHTTPRequest requestWithURLString: requestPath];
-	request.HTTPMethod = @"GET";
+	request.HTTPMethod = method;
+
+	if (payload)
+	{
+		if ([payload isKindOfClass: [NSData class]])
+			request.rawPOSTData = data = payload;
+		else
+		if (sendDictAsForm)
+		{
+			if ([payload isKindOfClass: [NSDictionary class]])
+				request.POSTDictionary = payload;
+			else
+				OBP_LOG(@"••• Payload needs to be a dictionary to send as a form; ignored: %@", payload);
+		}
+		else
+		if (serializeToJSON)
+		{
+			data = [NSJSONSerialization dataWithJSONObject: payload options: 0 error: &error];
+			OBP_LOG_IF(error || !data, @"Payload JSON serialize failed with error %@\n for data %@", error, payload);
+			if (data)
+			{
+				request.rawPOSTData = data;
+				moreHeaders[@"Content-Type"] = @"application/json";
+			}
+		}
+		else
+			OBP_LOG(@"••• Payload ignored: %@", payload);
+	}
+
+	if ([moreHeaders count])
+		[request.requestHeaders addEntriesFromDictionary: moreHeaders];
+
+	// Reply handler
 	request_ifStillAround = request;
     request.completionBlock = ^(NSDictionary *headers, NSString *body) {
 		STHTTPRequest *request = request_ifStillAround;
 		NSInteger status = request.responseStatus;
+		OBP_LOG_IF(verbose, @"\nResponse: %d\nHeaders: %@\nBody: %@", (int)status, headers, body);
 		NSError* error = nil;
 		BOOL handled = NO;
-        if (status == statusAcceptable)
+        if (NSNotFound != [acceptableStatusCodes indexOfObject: @(status)])
 		{
             id container = nil;
 			if (deserializeJSON)
 			{
 				container = [NSJSONSerialization JSONObjectWithData: [body dataUsingEncoding: NSUTF8StringEncoding] options: 0 error: &error];
 				OBP_LOG_IF(error, @"[NSJSONSerialization JSONObjectWithData: data options: 0 error:] gave error:\nerror = %@\ndata = %@", error, body);
-				OBP_LOG_IF(!error && xc && ![container isKindOfClass: xc], @"Expected to resource at path %@ to yield a %@, but got instead got:\n%@\nfrom body: %@", path, NSStringFromClass(xc), container, body);
-				if (!error && xc && ![container isKindOfClass: xc])
+				OBP_LOG_IF(!error && expectedDeserializedObjectClass && ![container isKindOfClass: expectedDeserializedObjectClass], @"Expected to resource at path %@ to yield a %@, but got instead got:\n%@\nfrom body: %@", path, NSStringFromClass(expectedDeserializedObjectClass), container, body);
+				if (!error && expectedDeserializedObjectClass && ![container isKindOfClass: expectedDeserializedObjectClass])
 					error = [NSError errorWithDomain: OBPMarshalErrorDomain code: OBPMarshalErrorUnexpectedResourceKind userInfo:@{NSLocalizedDescriptionKey:@"Unexpected response data type.",@"body":body?:@""}];
 			}
 			if (!error)
@@ -89,23 +241,62 @@
 		}
 		else
 		{
-			OBP_LOG(@"Unexpected response (%@); body = %@", @(status), body);
+			OBP_LOG(@"Unexpected response (%@), when expecting %@; body = %@", @(status), acceptableStatusCodes, body);
 			error = [NSError errorWithDomain: OBPMarshalErrorDomain code: OBPMarshalErrorUnexpectedResult userInfo:@{NSLocalizedDescriptionKey:[NSString stringWithFormat: @"Unexpected response status (%@).", @(status)],@"body":body?:@""}];
 		}
 		if (!handled)
 			eh(error, path);
 	};
-    
+
+	// Error handler
 	request.errorBlock = ^(NSError *error) {
         eh(error, path);
     };
 
-	if ([session authorizeSTHTTPRequest: request])
+	// Authorise and send
+	if (onlyPublicResources || [session authorizeSTHTTPRequest: request])
 	{
 		[request startAsynchronous];
+		OBP_LOG_IF(verbose, @"\n%@", NSStringDescribingNSURLRequest((NSURLRequest*)[(id)request performSelector: @selector(request)]));
 		return YES;
 	}
 
 	return NO;
 }
 @end
+
+
+
+NSString* NSStringDescribingNSURLRequest(NSURLRequest* request)
+{
+	// Snippet from: http://stackoverflow.com/a/31734423/618653
+	NSMutableString *message = [NSMutableString stringWithString:@"---Request------------------\n"];
+	[message appendFormat:@"URL: %@\n",[request.URL description] ];
+	[message appendFormat:@"Method: %@\n",[request HTTPMethod]];
+	for (NSString *header in [request allHTTPHeaderFields])
+	{
+		[message appendFormat:@"%@: %@\n",header,[request valueForHTTPHeaderField:header]];
+	}
+	[message appendFormat:@"Body: %@\n",[[NSString alloc] initWithData:[request HTTPBody] encoding:NSUTF8StringEncoding]];
+	[message appendString:@"----------------------------\n"];
+	return [NSString stringWithFormat:@"%@",message];
+};
+
+NSString* NSStringDescribingNSURLResponseAndData(NSHTTPURLResponse* response, NSData* data)
+{
+	// Snippet from: http://stackoverflow.com/a/31734423/618653
+	NSString *responsestr = [[NSString alloc] initWithBytes:[data bytes] length:[data length] encoding:NSUTF8StringEncoding];
+	NSMutableString *message = [NSMutableString stringWithString:@"---Response------------------\n"];
+	[message appendFormat:@"URL: %@\n",[response.URL description] ];
+	[message appendFormat:@"MIMEType: %@\n",response.MIMEType];
+	[message appendFormat:@"Status Code: %ld\n",(long)response.statusCode];
+	for (NSString *header in [[response allHeaderFields] allKeys])
+	{
+		[message appendFormat:@"%@: %@\n",header,[response allHeaderFields][header]];
+	}
+	[message appendFormat:@"Response Data: %@\n",responsestr];
+	[message appendString:@"----------------------------\n"];
+	return [NSString stringWithFormat:@"%@",message];
+};
+
+
